266966c8378a25ff4931d3e869088fc1d8cc6147
ruviRedner <r.y.r7966@gmail.com>
2024-07-24 13:03:19 +0300

    Add project files.

---
+++ DateWriting/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace DateWriting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbMonth = new System.Windows.Forms.ComboBox();
+            this.cmbYear = new System.Windows.Forms.ComboBox();
+            this.cmbDate = new System.Windows.Forms.ComboBox();
+            this.cmbDay = new System.Windows.Forms.ComboBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(383, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "כתיבת תאריך";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "שנה";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(305, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "חודש";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(460, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "היום בחודש";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(632, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "היום בשבוע";
+            // 
+            // cmbMonth
+            // 
+            this.cmbMonth.FormattingEnabled = true;
+            this.cmbMonth.Items.AddRange(new object[] {
+            "תשרי",
+            "מרחשוון",
+            "כסלו",
+            "טבת",
+            "שבט",
+            "אדר",
+            "אדר הראשון",
+            "אדר השני",
+            "ניסן",
+            "אייר",
+            "סיון",
+            "תמוז",
+            "אב",
+            "אלול"});
+            this.cmbMonth.Location = new System.Drawing.Point(255, 134);
+            this.cmbMonth.Name = "cmbMonth";
+            this.cmbMonth.Size = new System.Drawing.Size(121, 21);
+            this.cmbMonth.TabIndex = 5;
+            // 
+            // cmbYear
+            // 
+            this.cmbYear.FormattingEnabled = true;
+            this.cmbYear.Items.AddRange(new object[] {
+            "תשפד",
+            "תשפה",
+            "תשפו",
+            "תשפז",
+            "תשפח",
+            "תשפט"});
+            this.cmbYear.Location = new System.Drawing.Point(84, 134);
+            this.cmbYear.Name = "cmbYear";
+            this.cmbYear.Size = new System.Drawing.Size(121, 21);
+            this.cmbYear.TabIndex = 6;
+            // 
+            // cmbDate
+            // 
+            this.cmbDate.FormattingEnabled = true;
+            this.cmbDate.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30"});
+            this.cmbDate.Location = new System.Drawing.Point(430, 134);
+            this.cmbDate.Name = "cmbDate";
+            this.cmbDate.Size = new System.Drawing.Size(121, 21);
+            this.cmbDate.TabIndex = 7;
+            // 
+            // cmbDay
+            // 
+            this.cmbDay.FormattingEnabled = true;
+            this.cmbDay.Items.AddRange(new object[] {
+            "ראשון",
+            "שני",
+            "שלישי",
+            "רביעי",
+            "חמישי",
+            "ששי"});
+            this.cmbDay.Location = new System.Drawing.Point(603, 134);
+            this.cmbDay.Name = "cmbDay";
+            this.cmbDay.Size = new System.Drawing.Size(121, 21);
+            this.cmbDay.TabIndex = 8;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(349, 173);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 10;
+            this.btnCreate.Text = "יצירה";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(137, 271);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(41, 13);
+            this.lblResult.TabIndex = 11;
+            this.lblResult.Text = "תוצאה";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.cmbDay);
+            this.Controls.Add(this.cmbDate);
+            this.Controls.Add(this.cmbYear);
+            this.Controls.Add(this.cmbMonth);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "כתיבת תאריך";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbMonth;
+        private System.Windows.Forms.ComboBox cmbYear;
+        private System.Windows.Forms.ComboBox cmbDate;
+        private System.Windows.Forms.ComboBox cmbDay;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ DateWriting/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace DateWriting
+{
+    public partial class Form1 : Form
+    {
+        //הצהרת משתנה מסוג xml
+        XmlDocument XmlDocument;
+        //פה נמצא הנתיב של הקובץ
+        string patName = Directory.GetCurrentDirectory() + "//dateWritingData.xml";
+
+        string resultt;
+        public Form1()
+        {
+            InitializeComponent();
+            //יצירת מופע חדש מהקלאס xmldocuument
+            XmlDocument = new XmlDocument();
+            //אם הקובץ קיים במערכת
+            if (File.Exists(patName))
+            {
+                try
+                {
+                    //תטען לי אותו
+                    XmlDocument.Load(patName);
+                }
+                catch (Exception ex)
+                {
+                    //במקרה של שגיאה תשלח לי הודעת שגיאה
+                    MessageBox.Show("Error loading XML file" + ex.Message);
+                }
+
+            }
+            else
+            {
+                //במקרה שהקובץ לא קיים תיצור לי קובץ חדש והתגית הראשית תיקרא Querys
+                XmlNode root = XmlDocument.CreateElement("Querys");
+                //תוסיף את זה ל xml
+                XmlDocument.AppendChild(root);
+                //שמור בנתיב שכתבנו למעלה
+                XmlDocument.Save(patName);
+            }
+            
+        }
+
+        private void ClearAllFildis()
+
+        {
+            foreach (Control ctr in Controls)
+            {
+                if(ctr is ComboBox)
+                {
+                    ctr.Text = "";
+                }
+            }
+        }
+
+        private void SaveAsXml()
+        {
+            XmlDocument.Save(patName);
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            XmlNode Query = XmlDocument.CreateElement("drink");
+            //יצירת ילדים
+            Query.AppendChild(XmlDocument.CreateElement("Day")).InnerText = cmbDay.Text;
+            Query.AppendChild(XmlDocument.CreateElement("DayMonth")).InnerText = cmbDate.Text;
+            Query.AppendChild(XmlDocument.CreateElement("Month")).InnerText = cmbMonth.Text;
+            Query.AppendChild(XmlDocument.CreateElement("Year")).InnerText = cmbYear.Text;
+            Query.AppendChild(XmlDocument.CreateElement("Result")).InnerText = resultt;
+            
+
+            //מגדירה שהוא האבא 
+            XmlDocument.FirstChild.AppendChild(Query);
+            lblResult.Text = "";
+            Day(resultt);
+            DayMonth(resultt);
+            Year(resultt);
+            SaveAsXml();
+            
+        }
+
+        private void ShowDate()
+        {
+            lblResult.Text = "";
+
+        }
+
+        private string Day(string result)
+        {
+            switch(cmbDay.Text)
+            {
+                case "ראשון":
+                    resultt = " באחד בשבת";
+                    break;
+                case "שני":
+                    resultt = " בשני בשבת";
+                    break;
+                case "שלישי":
+                    resultt = "  בשלישי בשבת";
+                    break;
+                case "רביעי":
+                    resultt = " ברביעי בשבת";
+                    break;
+                case "חמישי":
+                    resultt = " בחמישי בשבת";
+                    break;
+                case "ששי":
+                    resultt = " בששי בשבת";
+                    break;
+            }
+            lblResult.Text += resultt;
+            return result;
+        }
+
+        private string DayMonth(string result)
+        {
+            
+            
+            switch(cmbDate.Text)
+            {
+              
+                case "1":
+                    resultt = " יום אחד לירח"; 
+                    break;
+                case "2":
+                    resultt = " שני ימים לירח";
+                    break;
+                case "3":
+                    resultt = "  שלושה ימים לירח";
+                    break;
+                case "4":
+                    resultt = " ארבעה ימים לירח";
+                    break;
+                case "5":
+                    resultt = " חמישה ימים לירח";
+                    break;
+                case "6":
+                    resultt = " ששה ימים לירח";
+                    break;
+                case "7":
+                    resultt = " שבעה ימים לירח";
+                    break;
+                case "8":
+                    resultt = " שמונה ימים לירח";
+                    break;
+                case "9":
+                    resultt = " תשעה ימים לירח";
+                    break;
+                case "10":
+                    resultt = " עשרה ימים לירח";
+                    break;
+                case "11":
+                    resultt = " אחד עשר יום לירח";
+                    break;
+                case "12":
+                    resultt = " שנים עשר יום לירח";
+                    break;
+                case "13":
+                    resultt = " שלשה עשר יום לירח";
+                    break;
+                case "14":
+                    resultt = " ארבעה עשר יום לירח";
+                    break;
+                case "15":
+                    resultt = " חמשה עשר יום לירח";
+                    break;
+                case "16":
+                    resultt = " ששה עשר יום לירח";
+                    break;
+                case "17":
+                    resultt = " שבעה עשר יום לירח";
+                    break;
+                case "18":
+                    resultt = " שמנה עשר יום לירח";
+                    break;
+                case "19":
+                    resultt = " תשעה עשר יום לירח";
+                    break;
+                case "20":
+                    resultt = " עשרים יום לירח";
+                    break;
+                case "21":
+                    resultt = " אחד ועשרים יום לירח";
+                    break;
+                case "22":
+                    resultt = " שנים ועשרים יום לירח";
+                    break;
+                case "23":
+                    resultt = " שלשה ועשרים יום לירח";
+                    break;
+                case "24":
+                    resultt = " ארבעה ועשרים יום לירח";
+                    break;
+                case "25":
+                    resultt = " חמשה ועשרים יום";
+                    break;
+                case "26":
+                    resultt = " ששה ועשרים יום";
+                    break;
+                case "27":
+                    resultt = " שבעה ועשרים יום";
+                    break;
+                case "28":
+                    resultt = " שמנה ועשרים יום";
+                    break;
+                case "29":
+                    resultt = " תשעה ועשרים יום";
+                    break;
+                if  (cmbDate.Text == "30")
+                    {
+                        switch (cmbMonth.Text)
+                        {
+                            case "תשרי":
+                                resultt = "יום שלשים לחדש תשרי שהוא ראש חדש מרחשוון";
+                                break;
+                            case "מרחשוון":
+                                resultt = "יום שלשים לחדש מרחשוון שהוא ראש חדש כסלו";
+                                break;
+                            case "כסלו":
+                                resultt = "יום שלשים לחדש כסלו שהוא ראש חדש טבת";
+                                break;
+                            case "טבת":
+                                resultt = "יום שלשים לחדש טבת שהוא ראש חדש שבט";
+                                break;
+                            case "שבט":
+                                resultt = "יום שלשים לחדש שבט שהוא ראש חדש אדר";
+                                break;
+                            case "אדר":
+                                resultt = "יום שלשים לחדש אדר שהוא ראש חדש אדר הראשון";
+                                break;
+                            case "אדר הראשון":
+                                resultt = "יום שלשים לחדש אדר הראשון שהוא ראש חדש אדר השני";
+                                break;
+                            case "אדר השני":
+                                resultt = "יום שלשים לחדש אדר השני שהוא ראש חדש ניסן";
+                                break;
+                            case "ניסן":
+                                resultt = "יום שלשים לחדש ניסן שהוא ראש חדש אייר";
+                                break;
+                            case "אייר":
+                                resultt = "יום שלשים לחדש אייר שהוא ראש חדש סיון";
+                                break;
+                            case "סיון":
+                                resultt = "יום שלשים לחדש סיון שהוא ראש חדש תמוז";
+                                break;
+                            case "תמוז":
+                                resultt = "יום שלשים לחדש תמוז שהוא ראש חדש אב";
+                                break;
+                            case "אב":
+                                resultt = "יום שלשים לחדש אב שהוא ראש חדש אלול";
+                                break;
+                            case "אלול":
+                                resultt = "יום שלשים לחדש אלול שהוא ראש חדש תשרי";
+                                break;
+                        }
+                    }
+                lblResult.Text += resultt;
+                    return resultt;
+                   
+                        
+               
+                    
+                    
+                
+            }
+            lblResult.Text += resultt;
+            return result;
+        }
+        
+
+        private string Year(string result)
+        {
+            switch(cmbYear.Text)
+            {
+                case "תשפד":
+                    resultt = "בשנת חמשת אלפים שבע מאות שמנים וארבע לבריאת העולם";
+                    break;
+                case "תשפה":
+                    resultt = "בשנת חמשת אלפים שבע מאות שמנים וחמש לבריאת העולם";
+                    break;
+                case "תשפו":
+                    resultt       = "בשנת חמשת אלפים שבע מאות שמנים ושש לבריאת העולם";
+                    break;
+                case "תשפז":
+                    resultt = "בשנת חמשת אלפים שבע מאות שמנים ושבע לבריאת העולם";
+                    break;
+                case "תשפח":
+                    resultt = "  בשנת חמשת אלפים שבע מאות שמנים ושמנה לבריאת העולם";
+                    break;
+                case "תשפט":
+                    resultt = "בשנת חמשת אלפים שבע מאות שמנים ותשע לבריאת העולם";
+                    break;
+            }
+            lblResult.Text += resultt;
+            return result;
+        }
+    }
+}
